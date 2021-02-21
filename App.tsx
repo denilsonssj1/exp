47d1126303feeaa6fba14bd48ddff9c59de97b54
Name <email@email.com>
2021-02-21 17:53:36 -0300

    Add support to .env files

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'dotenv/config';
 
 export default function App() {
+  const [code, setCode] = useState<string>('');
+
+  useEffect(()=> {
+    const code = process.env.CODE;
+    setCode(code? code : '');
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Code { code }</Text>
       <StatusBar style="auto" />
     </View>
   );
